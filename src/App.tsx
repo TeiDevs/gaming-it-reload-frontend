1567216b94d2fb1ee8931f7ef449f8159fb3f61f
Elena Sacramento <elenasacramento80@gmail.com>
2022-11-07 18:48:48 +0000

    Add scaffolding and primereact 🚀

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'primereact/button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        
+      <Button label="Save" />
+ 
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
